e6091207368478ad434d8ff440b64bc1882c4614
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-03-02 16:10:18 +0530

    Made the cart button and its backdrop to work on click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+
+
 function App() {
+  const [cartShow, setCartShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartShow(true);
+  }
+
+  const hideCartHandler = () => 
+  {
+    setCartShow(false);
+  }
 
   return (
     <Fragment>
-      <Cart></Cart>
-      <Header/>
+      {cartShow && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
         <main>
           <Meals/>
         </main>
